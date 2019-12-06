cf828d932ff69df2ab4191a17149ee7bb9810b73
miaoxijun <>
2019-12-06 20:51:58 +0800

    first

---
+++ class/XJNativeModuleExample.h
@@ -0,0 +1,13 @@
+//
+//  XJNativeModuleExample.h
+//  XJNativeModuleExample
+//
+//  Created by miaoxijun on 2019/12/6.
+//  Copyright © 2019 miaoxijun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "RCTBridgeModule.h"
+@interface XJNativeModuleExample : NSObject
+
+@end
---
+++ class/XJNativeModuleExample.m
@@ -0,0 +1,13 @@
+//
+//  XJNativeModuleExample.m
+//  XJNativeModuleExample
+//
+//  Created by miaoxijun on 2019/12/6.
+//  Copyright © 2019 miaoxijun. All rights reserved.
+//
+
+#import "XJNativeModuleExample.h"
+
+@implementation XJNativeModuleExample
+
+@end
